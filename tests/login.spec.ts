bda25935490fd9613ce06f680bfe57baf9a9fe27
aska <j_miastkowska@o2.pl>
2023-12-28 21:26:55 +0100

    login tests

---
+++ tests/login.spec.ts
@@ -0,0 +1,40 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('the user login to Demobank', () => {
+
+  test('login with correct credentials', async ({ page }) => {
+    await page.goto('https://demo-bank.vercel.app/');
+    await page.getByTestId('login-input').click();
+    await page.getByTestId('login-input').fill('tester12');
+    await page.getByTestId('password-input').click();
+    await page.getByTestId('password-input').fill('testtest');
+    await page.getByTestId('login-button').click();
+
+    await expect(page.getByTestId('user-name')).toHaveText('Jan Demobankowy');
+  });
+
+
+
+  test('login with incorrect user name', async ({ page }) => {
+    await page.goto('https://demo-bank.vercel.app/');
+    await page.getByTestId('login-input').click();
+    await page.getByTestId('login-input').fill('teste');
+    await page.getByTestId('password-input').click();
+
+    await expect(page.getByTestId('error-login-id')).toHaveText('identyfikator ma min. 8 znaków');
+
+  });
+
+  test('unsuccessful login with to short password', async ({ page }) => {
+    await page.goto('https://demo-bank.vercel.app/');
+    await page.getByTestId('login-input').click();
+    await page.getByTestId('login-input').fill('tester12');
+    await page.getByTestId('password-input').click();
+    await page.getByTestId('password-input').fill('tet');
+    await page.getByRole('heading', { name: 'Wersja demonstracyjna serwisu' }).click();
+
+    await expect(page.getByTestId('error-login-password')).toHaveText('hasło ma min. 8 znaków');
+
+  });
+
+});
